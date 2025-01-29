9933ec035e2cd6e17c67802e12e6ec2da57d01c0
shazinaHijazy97 <zahrashazina97@gmail.com>
2025-01-29 15:43:54 -0500

    Usestate and Add task Button

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './TaskList.css';
+import { useState } from 'react';
 
 function TaskList() {
-  const tasks = ['Buy groceries', 'Study React', 'Prepare CV', 'Apply for Jobs', 
-    'Read on Text CSS styling', 'Volunteering', 'Look for Mentors','Go for a Walk', 'Read the Book'];
+  const [tasks, setTasks] = useState(['Buy groceries', 'Prepare CV', 'Apply for Jobs', 
+    'Read on Text CSS styling', 'Volunteering', 'Look for Mentors','Go for a Walk', 'Read the Book']);
+
+    const [newTask, setNewTask] = useState("");
+
+    const handleInputChange = (e) => {
+      setNewTask(e.target.value);
+    };
+
+      const addTask = () => {
+        if (newTask.trim() !== "") { // Prevent empty tasks, trim checks for spaces, ignores leading and trailing spaces leaving the spaces between the content 
+          setTasks([...tasks, newTask]); // Add task to the list, ...tasks ensures the prevoius tasks are not changed
+          setNewTask(""); // Clear input field
+        }
+      };
+
 
   return (
     <div>
       <h2>Task List</h2>
-      <ul className = "task-list">
+        <input type= "text"
+        value= {newTask}
+        onChange ={handleInputChange}
+        placeholder="Enter a New Task"/>
+
+        <button onClick={addTask}>Add Task</button>
+
+        <ul className = "task-list">
         {tasks.map((task, index) => (
           <li key={index} className = "task-item">{task}</li>
         ))}
